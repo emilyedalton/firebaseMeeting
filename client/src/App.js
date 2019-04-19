048d3232a37e859e77bd896f739d5a388f93e5f2
Emily Dalton <emilydalton@dhcp-10-105-33-155.wireless.northwestern.private>
2019-04-19 15:00:01 -0500

    adding a state 'user'

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,16 @@ class App extends Component {
 constructor(){
   super();
   this.state = {
-    user: 'bobby'
+    user: 'emily'
   }
 }
 
-
+// we have created a state. capturing the name of the variable in a property. 
   render() {
     return (
       <div className="App">
         <h1>Meeting Log</h1>
-        <Home />
+        <Home user ={this.state.user} />
       </div>
     );
   }
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -7,9 +7,15 @@ render(){
     return(
 <div>
 
-    <h2>hello Hi</h2>
+    <h2>hello {user}</h2>
+
+    { user == null && ( 
+        <div>
     <button type="button" className="btn btn-success">Success</button>
     <button type="button" className="btn btn-danger">Danger</button>
+    </div>)}
+    {user && ( <div><button type="button" className="btn btn-warning">Warning</button></div>
+    )}
 </div>
 
     )
